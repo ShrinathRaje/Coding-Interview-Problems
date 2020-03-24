e1135c07a27450c53c568bff33c63c18dcb7bdc9
Shrinath <developer.shrinath@gmail.com>
2020-03-24 22:36:38 +0530

    heap sort

---
+++ heap/heap_sort.cpp
@@ -0,0 +1,85 @@
+/*
+* Heap sort
+* Author: Shrinath Bhosale
+* Date: 24'th March 2020
+* Problem statement: https://practice.geeksforgeeks.org/problems/heap-sort/1/
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void heapify(int arr[], int n, int i);
+
+void buildHeap(int arr[], int n);
+
+void heapSort(int arr[], int n)
+{
+    buildHeap(arr, n);
+    for (int i = n - 1; i >= 0; i--)
+    {
+        swap(arr[0], arr[i]);
+        heapify(arr, i, 0);
+    }
+}
+
+void printArray(int arr[], int size)
+{
+    int i;
+    for (i = 0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+
+int main()
+{
+    int arr[1000000], n, T, i;
+    scanf("%d", &T);
+    while (T--)
+    {
+        scanf("%d", &n);
+        for (i = 0; i < n; i++)
+            scanf("%d", &arr[i]);
+        heapSort(arr, n);
+        printArray(arr, n);
+    }
+    return 0;
+}
+
+void heapify(int arr[], int n, int i)
+{
+
+    int largest = i;
+    int l = 2 * i + 1;
+    int r = 2 * i + 2;
+
+    if (l < n && arr[l] > arr[largest])
+        largest = l;
+
+    if (r < n && arr[r] > arr[largest])
+        largest = r;
+
+    if (largest != i)
+    {
+        int t = arr[i];
+        arr[i] = arr[largest];
+        arr[largest] = t;
+
+        heapify(arr, n, largest);
+    }
+}
+
+void buildHeap(int arr[], int n)
+{
+    for (int i = 1; i < n; ++i)
+    {
+        int j = i;
+
+        while (j > 0 && arr[j] > arr[(j - 1) / 2])
+        {
+            int temp = arr[j];
+            arr[j] = arr[(j - 1) / 2];
+            arr[(j - 1) / 2] = temp;
+            j = (j - 1) / 2;
+        }
+    }
+}
