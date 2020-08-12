582a1d08e53cb0cba9186a2e21575bb5ec9dca52
Shrinath Bhosale <developer.shrinath@gmail.com>
2020-08-12 17:08:32 +0530

    sort the linked list of 0s 1s and 2s

---
+++ linked_list/sortLinkedList.cpp
@@ -0,0 +1,145 @@
+//sort the linked list of 0s 1s and 2s
+//problem: https://practice.geeksforgeeks.org/problems/given-a-linked-list-of-0s-1s-and-2s-sort-it/1/
+
+// { Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+/* Link list Node */
+struct Node {
+    int data;
+    struct Node *next;
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+struct Node *start = NULL;
+
+struct Node* segregate(struct Node *head);
+
+// Function to sort a linked list of 0s, 1s and 2s
+void printList(struct Node *Node) {
+    while (Node != NULL) {
+        printf("%d ", Node->data);
+        Node = Node->next;
+    }
+    printf("\n");
+}
+
+/* Drier program to test above function*/
+void insert(int n1) {
+    int n, value, i;
+    // scanf("%d",&n);
+    n = n1;
+    struct Node *temp;
+    for (i = 0; i < n; i++) {
+        scanf("%d", &value);
+
+        if (i == 0) {
+            start = new Node(value);
+            temp = start;
+            continue;
+        } else {
+            temp->next = new Node(value);
+            temp = temp->next;
+            temp->next = NULL;
+        }
+    }
+}
+
+int main() {
+
+    int n;
+
+    int t;
+    scanf("%d", &t);
+
+    while (t--) {
+        scanf("%d", &n);
+
+        insert(n);
+        struct Node *newHead = segregate(start);
+        printList(newHead);
+    }
+
+    return 0;
+}// } Driver Code Ends
+
+
+/*
+  Sort the list of 0's,1's and 2's
+  The input list will have at least one element
+  Node is defined as
+  struct Node {
+    int data;
+    struct Node *next;
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+*/
+
+// This function is to segregate the elememtns in the linked list
+// This will do the required arrangement by changing the links
+Node* segregate(Node *head) {
+    //method 1
+    //changing data
+
+	// int a[3] = {0};
+ //    Node *p = head;
+ //    while(p) {
+ //    	++a[p->data];
+ //    	p = p->next;
+ //    }
+
+ //    p = head;
+ //    for(int i=0; i<3; ++i) {
+ //    	while(a[i]--) {
+ //    		p->data = i;
+ //    		p=p->next;
+ //    	}
+ //    }
+
+ //    return head;
+
+    //method 2
+    //changing links
+
+    Node *zd = new Node(0);
+    Node *od = new Node(0);
+    Node *td = new Node(0);
+
+    Node *z = zd, *o = od, *t = td;
+
+    Node *p = head;
+    while(p) {
+    	if(p->data == 0) {
+    		z->next = p;
+    		z = z->next;
+    	} else if(p->data == 1) {
+    		o->next = p;
+    		o = o->next;
+    	} else {
+    		t->next = p;
+    		t = t->next;
+    	}
+
+    	p = p->next;
+    }
+
+    z->next = (od->next) ? od->next : td->next;
+    o->next = td->next;
+    t->next = NULL;
+
+    head = zd->next;
+
+    delete zd;
+    delete od;
+    delete td;
+
+    return head;
+}
